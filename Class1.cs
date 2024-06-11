54ce8aff138bc75e33f238acac5828bc8d1d5acc
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-06-11 13:03:04 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit
+{
+    internal class Class1
+    {
+        string ime, prezime, uloge, aktivnost;
+
+        public Class1(string Ime, string Prezime, string Uloge)
+        {
+            this.Ime = Ime;
+            this.Prezime = Prezime;
+            this.uloge = Uloge;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "ime: " + this.Ime
+                + "\tprezime: " + this.Prezime
+                + "\tuloge: " + this.uloge
+                + "\taktivnost: " + this.aktivnost
+                + "\r\n";
+            return ispis;
+        }
+
+
+        public string Ime { get => Ime; set => Ime = value; }
+        public string Prezime { get => Prezime; set => Prezime = value; }
+        public string Uloge { get => Uloge; set => Uloge = value; }
+        public string Aktivnost { get => Aktivnost; set => Aktivnost = value; }
+    }
+}
---
+++ Ispit.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ispit
+{
+    public partial class Form1 : Form
+    {
+        List<Class1> Class1List = new List<Class1>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIme.Text == "" ||
+                    txtPrezime.Text == "" ||
+                    cmbUloge.Text == "")
+                {
+                    MessageBox.Show("Pogrešan unos. Molimo pokušajte ponovo",
+                                        "Pogrešan unos", MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                    txtIme.Clear();
+                    txtPrezime.Clear();
+                    txtIme.Focus();
+                }
+                else
+                {
+                    Class1 class1 = new Class1(txtIme.Text,
+                        txtPrezime.Text,
+                        cmbUloge.Text);
+                    Class1List.Add(class1);
+                    txtIme.Clear();
+                    txtPrezime.Clear();
+                    txtIme.Focus();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Pogrešan unos. Molimo pokušajte ponovo",
+                    "Pogrešan unos", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtIme.Clear();
+                txtPrezime.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+}
+
